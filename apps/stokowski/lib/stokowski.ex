5c12085a22f3a54e7e561519b9396baa0d7596ab
Tj (bougyman) Vanderpoel <bougyman@hey.com>
2026-08-15 15:17:05 -0500

    initial commit of plan and vendored submodules

---
+++ apps/stokowski/lib/stokowski.ex
@@ -0,0 +1,18 @@
+defmodule Stokowski do
+  @moduledoc """
+  Documentation for `Stokowski`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Stokowski.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/stokowski/test/stokowski_test.exs
@@ -0,0 +1,8 @@
+defmodule StokowskiTest do
+  use ExUnit.Case
+  doctest Stokowski
+
+  test "greets the world" do
+    assert Stokowski.hello() == :world
+  end
+end
